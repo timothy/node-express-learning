f8080fe733fdf917893aa21a7c4c84fd41abc146
tabradford <tabradford@liberty.edu>
2016-04-12 14:11:53 -0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by tabradford on 3/23/2016.
+ */
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+   return gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }))
+        .pipe(jscs());
+});
+
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+
+});
