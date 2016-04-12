f5fc9684c7691832ae9048e1913d53c8a14411f9
tabradford <tabradford@liberty.edu>
2016-04-12 16:26:23 -0400

    Testing out handlebars. This is the start of a simple handlebars test app.

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.set('views', './src/views');
-app.set('view engine', 'jade');
+
+var handlebars = require('express-handlebars');
+app.engine('.hbs', handlebars({extname:'.hbs'}));
+
+app.set('view engine', '.hbs');
 
 app.get('/', function (req, res) {
     //res.send('Hellow World');
