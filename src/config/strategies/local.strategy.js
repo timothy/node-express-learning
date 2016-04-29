c3e547a4eeef8dad13f90fe901e88d93d5feb925
tabradford <tabradford@liberty.edu>
2016-04-29 15:09:45 -0400

    Added pass verification for sign in.

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -3,17 +3,34 @@
  */
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var mongodb = require('mongodb').MongoClient;
 
 module.exports = function () {
-  passport.use(new LocalStrategy({
-      usernameField:'userName',
-      passwordField:'password'
-  },
-  function (username, password, done) {
-      var user = {
-          username: username,
-          password: password
-      };
-      done(null, user);
-  }))
+    passport.use(new LocalStrategy({
+            usernameField: 'userName',
+            passwordField: 'password'
+        },
+        function (username, password, done) {
+            var url = 'mongodb://localhost:27017/libraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                collection.findOne({username: username},
+                    function (err, results) {
+                        if(results.password === password){
+                            var user = results;
+                            done(null, user);
+                        } else {
+                            done('Bad password', false,{message:'Bad Password'});
+                        }
+
+                    });
+            });
+
+            var user = {
+                username: username,
+                password: password
+            };
+            done(null, user);
+        }))
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,3 @@
-/**
- * Created by  on 4/27/2016.
- */
 var express = require('express');
 var authRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
@@ -9,9 +6,21 @@ var router = function () {
     authRouter.route('/signUp')
         .post(function (req, res) {
             console.log(req.body);
-            req.login(req.body, function () {
-                res.redirect('/auth/profile');
-            })
+            var url = 'mongodb://localhost:27017/libraryApp';
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('users');
+                var user = {
+                    username: req.body.userName,
+                    password: req.body.password
+                };
+
+                collection.insert(user, function (err, results) {
+                    req.login(results.ops[0], function () {
+                        res.redirect('/auth/profile');
+                    });
+                });
+            });
+
         });
     authRouter.route('/profile')
         .get(function (req, res) {
