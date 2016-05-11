2f79c71c2ff35db27df4c6f302f4e6d3b17d0e94
tabradford <tabradford@liberty.edu>
2016-05-11 15:40:55 -0400

    added a controller for the bookRoutes

---
+++ src/controllers/bookController.js
@@ -0,0 +1,58 @@
+/**
+ * Created by tabradford on 5/11/2016.
+ */
+var mongodb = require('mongodb').MongoClient;
+var objectID = require('mongodb').ObjectID;
+
+var bookController = function (bookService, nav) {
+
+    var middleware = function (req, res, next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    };
+
+    var getIndex = function (req, res) {
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+
+            collection.find({}).toArray(function (err, results) {
+                res.render('bookListView', {
+                    title: 'Books',
+                    nav: nav,
+                    books: results
+                });
+            });
+        });
+    };
+
+    var getById = function (req, res) {
+        var id = new objectID(req.params.id);
+        var url = 'mongodb://localhost:27017/libraryApp';
+
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+
+            collection.findOne({_id: id}, function (err, results) {
+                res.render('bookView', {
+                    title: 'Books',
+                    nav: nav,
+                    book: results
+                });
+            });
+        });
+
+    };
+
+
+    return {
+        getIndex: getIndex,
+        getById: getById,
+        middleware: middleware
+    }
+};
+
+module.exports = bookController;
